474dee83d066842a66e80cbcbf2b89aadb49dd34
Mynor2717 <126991094+Mynor2717@users.noreply.github.com>
2024-01-02 22:54:53 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+ const user = {
+    "name": "Mynor",
+    "lastname": "Parrales",
+
+}
+
+//console.log(JSON.stringify(user));
+
+const friend = [
+    {"name": "joe", "nickname": "joe123"},
+    {"name": "marcus", "nickname": "joe124453"},
+    {"name": "luis", "nickname": "joe423"}
+
+]
+
+//console.log(user);
+console.log(JSON.stringify(friend));
